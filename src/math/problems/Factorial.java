36be522dfc6e99d0cc75bdbe1a7ea0f15abccab4
[Msaleemullah] <[sarimwali689@gmail.com]>
2018-09-10 13:20:16 -0400

    seventh-commit

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -12,5 +12,13 @@ public class Factorial {
          *
          */
 
+
+        int i,fact=1;
+        int number=5;//It is the number to calculate factorial
+        for(i=1;i<=number;i++){
+            fact=fact*i;
+        }
+        System.out.println("Factorial of "+number+" is: "+fact);
     }
 }
+
